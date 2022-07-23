96f3a198be744999849a457e4a695a64450ad5f0
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-07-24 03:52:56 +0530

    Learn loops

---
+++ conditional-loop/code/src/com/deepak/Loops.java
@@ -0,0 +1,23 @@
+package com.deepak;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+//        For Loops
+        for (int num=1; num <=5; num +=1){
+            System.out.println(num);
+        }
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0;i <= n; i++){
+            System.out.println(i);
+        }
+
+//        While Loops
+
+
+    }
+}
--- conditional-loop/code/src/com/deepak/Main.java
+++ conditional-loop/code/src/com/deepak/Main.java
@@ -3,11 +3,20 @@ package com.deepak;
 public class Main {
 
     public static void main(String[] args) {
-	    int salary = 25400;
+//	    int salary = 25400;
+//        if (salary > 10000){
+//            salary = salary + 2000;
+//        }else{
+//            salary = salary +10000;
+//        }
+
+        int salary = 19000;
         if (salary > 10000){
-            salary = salary + 2000;
-        }else{
-            salary = salary +10000;
+            salary += 2000;
+        }else if(salary > 20000) {
+            salary += 10000;
+        }else {
+            salary += 2000;
         }
         System.out.println(salary);
     }
