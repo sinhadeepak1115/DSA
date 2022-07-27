b8be996f9d6f6b6b99b02ffdc5ac904dc8933aef
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-07-27 20:12:21 +0530

    learnig ceiling and floor in binary search

--- binary-search-algo/code/src/com/deepak/Floor.java
+++ binary-search-algo/code/src/com/deepak/Floor.java
@@ -1,32 +1,29 @@
 package com.deepak;
 
-public class Ceiling {
+public class Floor {
     public static void main(String[] args) {
-
         int[] arr = {2, 3, 5, 9, 14, 16, 18};
         int target = 15;
-        int ans = ceiling(arr, target);
+        int ans = floor(arr, target);
         System.out.println(ans);
+
     }
-    static int ceiling(int arr[], int target){
+    static int floor(int[] arr , int target ){
         if ( target > arr[arr.length - 1] ){
             return -1;
         }
         int start = 0;
         int end = arr.length - 1;
-        while ( start <= end){
-            int mid = start + (end - start) / 2;
-            if ( target < arr[mid] ){
+        while ( start <= end ){
+            int mid = start + ( end - start ) / 2;
+            if ( target < arr[mid]){
                 end = mid - 1;
-            }else if( target > arr[mid] ){
+            }else if ( target > arr[mid]){
                 start = mid + 1;
-            }else {
+            }else{
                 return mid;
             }
         }
-//        Floor of a number
-//        return end;
-//        Ceiling of a number
-        return start;
+        return end;
     }
 }
